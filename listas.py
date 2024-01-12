a4cc3a313ef41e5ce5384e8dce8cf903a8a2075e
Marley Paranhos <mpsvideos.marley@gmail.com>
2024-01-12 17:27:57 -0300

    Add files via upload
    desafio curso aws_pros

---
+++ listas.py
@@ -0,0 +1,18 @@
+"""
+Curso AWS PROZ
+Turma: 20
+Aluno: Marley Paranhos
+Declare dois arrays, cada um com um mínimo de cinco elementos, e imprima eles no terminal usando o comando print().
+O primeiro array deve conter os produtos de uma loja da sua escolha (loja de comida, materiais de construção, música, etc).
+O segundo array deve conter os anos de nascimento de familiares e amigos seus.
+Lembre-se de usar nomes descritivos para nomear cada variável, e de usar o tipo de dado apropriado
+para cada lista (strings, booleanos, números inteiros, floats).
+"""
+
+# Declare dois arrays, cada um com um mínimo de cinco elementos
+material_construcao = ['cimento', 'areia', 'tijolo', 'telha', 'brita']
+ano_nascimento = [1999, 1945, 1947, 1976, 2015]
+
+# imprima eles no terminal usando o comando print()
+print(material_construcao)
+print(ano_nascimento)
